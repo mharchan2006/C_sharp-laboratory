af85464b43e698b1b0a1cf27e9a48950fa1c4161
harini <mharchan2006@gmail.com>
2026-09-17 21:33:14 +0530

    added exp

---
+++ exp6/Program6.cs
@@ -0,0 +1,38 @@
+﻿using System;
+public delegate void Notify();
+class Publisher
+{
+public event Notify OnProcessCompleted;
+public void StartProcess()
+    {
+        Console.WriteLine("Process Started...");
+        Console.WriteLine("Process Completed.");
+
+        if (OnProcessCompleted != null)
+        {
+            OnProcessCompleted();
+        }
+    }
+}
+
+
+class Subscriber
+{
+    public void ShowMessage()
+    {
+        Console.WriteLine("Event Received: Task Completed Successfully.");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Publisher obj = new Publisher();
+        Subscriber sub = new Subscriber();
+
+        obj.OnProcessCompleted += sub.ShowMessage;
+        obj.StartProcess();
+        Console.ReadKey();
+    }
+}
+
---
+++ exp7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Exp7
+{
+    public class MyForm : Form
+    {
+        Button button;
+
+        public MyForm()
+        {
+            button = new Button();
+
+            button.Text = "Click Me";
+            button.Location = new Point(100, 80);
+
+            button.Click += Button_Click;
+
+            Controls.Add(button);
+
+            Text = "My First Form";
+            Width = 300;
+            Height = 200;
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World!");
+        }
+    }
+
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.Run(new MyForm());
+        }
+    }
+}
